89674b287283cfcb4c54c106f2836434c37cdb84
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2023-11-23 21:26:58 +0700

    feat: set up eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_PORT) ?? 5173
+    },
+    css: {
+      devSourcemap: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  })
+}
